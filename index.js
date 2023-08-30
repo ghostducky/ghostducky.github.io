fce39e8707c889c870d279c887e303a76ea3a344
onnikeuda <onni.raappana@edu.keuda.fi>
2023-08-30 10:32:11 +0300

    added basic stuff

--- index.js
+++
@@ -1,8 +0,0 @@
-function rekisteroi() {
-    let nimi = document.getElementById("kayttajanimi").value;
-    let salasana = document.getElementById("salasana").value;
-
-    localStorage.setItem("kayttajanimi", nimi)
-    localStorage.setItem("salasana", salasana)
-
-}
