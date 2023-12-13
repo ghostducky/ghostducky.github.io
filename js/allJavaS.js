6af010684b7b77af4a23973fa4320fe208bfe288
onnikeuda <raappanao22@gmail.com>
2023-12-13 22:57:14 +0200

    something broke, will fix some day

---
+++ js/allJavaS.js
@@ -0,0 +1,9 @@
+function navToFi() {
+    window.location.href ='/fi/home.html';
+}
+function navToEn() {
+    windows.location.href = '/home.html';
+}
+function navToCon() {
+    windows.location.href = '/en/contact.html';
+}
