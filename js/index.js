80fb67771164d5c3655b2d5dfff623e809a818f6
onnikeuda <raappanao22@gmail.com>
2023-12-13 22:22:35 +0200

    removed

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function switchLanguage(event) {
-    const langButtons = document.querySelectorAll('.lang');
-    langButtons.forEach(button => button.classList.remove('selected'));
-
-    const selectedButton = event.target;
-    selectedButton.classList.add('selected');
-    // Add language-switching logic here
-}
