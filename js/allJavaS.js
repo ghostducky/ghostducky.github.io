093002a0c0525e154e39b51f919a8b3f66165080
onnikeuda <raappanao22@gmail.com>
2024-04-08 12:24:55 +0300

    deleted nav js

--- js/allJavaS.js
+++ js/allJavaS.js
@@ -1,12 +1,3 @@
-function navToFi() {
-    window.location.href ='/fi/home.html';
-}
 function navToEn() {
     window.location.href = '/home.html';
-}
-function navToCon() {
-    window.location.href = '/en/contact.html';
-}
-function navToOthr() {
-    window.location.href = '/en/other-work.html';
 }
