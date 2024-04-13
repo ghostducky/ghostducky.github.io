b0b4212548c37befb9db07a09db84f8fefc37585
onnikeuda <raappanao22@gmail.com>
2024-04-14 01:20:37 +0300

    theme change, WIP

--- js/allJavaS.js
+++ js/allJavaS.js
@@ -1,3 +1,26 @@
 function navToEn() {
     window.location.href = '/home.html';
 }
+
+const toggleBtn = document.querySelector('.container');
+
+toggleBtn.addEventListener('click', () => {
+    const body = document.body;
+    const currentTheme = body.classList.contains('dark-theme') ? 'dark-theme' : 'white-theme';
+    const newTheme = currentTheme === 'dark-theme' ? 'white-theme' : 'dark-theme';
+
+    body.classList.remove(currentTheme);
+    body.classList.add(newTheme);
+
+    // Update CSS variables for background color and text color based on the selected theme
+    const root = document.documentElement;
+    if (newTheme === 'dark-theme') {
+        root.style.setProperty('--background-color', 'var(--dark-upDown-theme)');
+        root.style.setProperty('--text-color', 'var(--white-upDown-theme)');
+        root.style.setProperty('--background-main-color', 'var(--dark-main-theme)');
+    } else {
+        root.style.setProperty('--background-color', 'var(--white-upDown-theme)');
+        root.style.setProperty('--text-color', 'var(--dark-upDown-theme)');
+        root.style.setProperty('--background-main-color', 'var(--white-main-theme)');
+    }
+});
