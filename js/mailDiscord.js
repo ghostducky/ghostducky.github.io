1e5772d1430052818c105def28f18103a7b2a229
onnikeuda <raappanao22@gmail.com>
2024-04-16 14:34:28 +0300

    a better error message

--- js/mailDiscord.js
+++ js/mailDiscord.js
@@ -1,3 +1,5 @@
+document.getElementById('send').addEventListener('click', submitForm);
+
 function isValidEmail(email) {
     // Regular expression for validating an Email
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -8,15 +10,20 @@ function submitForm() {
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
+    const errorMessage = document.getElementById('error-message');
 
     if (!isValidEmail(email)) {
-            alert('Please enter a valid email address.');
-            return;
+        errorMessage.textContent = 'Please enter a valid email address.';
+        errorMessage.style.opacity = '100%';
+        return;
     }
+    errorMessage.textContent = '';
+    errorMessage.style.opacity = '0%';
 
     const webhookUrl = 'https://discord.com/api/webhooks/1229360738313830471/e59WUS8-MRO0oTcUlGwHybdGjIRE-xyuRTfCAu9gQEW0CrwIVsGKNXtO9CGoxIb0tI1m';
 
     const payload = {
+            content: "<@614014094000783370>",
             embeds: [
                 {
                     title: "**You Have a New Message FROM: Portfolio**",
@@ -39,7 +46,7 @@ function submitForm() {
                     ],
                 },
             ],
-    };    
+    };
 
     fetch(webhookUrl, {
         method: 'POST',
@@ -50,13 +57,24 @@ function submitForm() {
     })
     .then(response => {
         if (!response.ok) {
-            throw new Error('Failed to send message to Discord');
+            errorMessage.textContent = 'Failed to send message to Discord.';
+            errorMessage.style.opacity = '100%';
+            return;
         }
-        alert('Message has been sent!');
+        errorMessage.textContent = 'Message sent successfully!';
+        errorMessage.style.opacity = '100%';
+
+        setTimeout(() => {
+            errorMessage.textContent = '';
+            errorMessage.style.opacity = '0%';
+        }, 3000);
+
         document.getElementById('contact-form').reset();
     })
     .catch(error => {
         console.error(error);
-        alert('Failed to send the message. Try again later.');
+        errorMessage.textContent = 'Please enter a valid email address.';
+        errorMessage.style.opacity = '100%';
+        return;
     });
 }
