44f459cfe640200d0ebd2ca67e6f478b059914a3
onnikeuda <raappanao22@gmail.com>
2024-04-19 11:07:34 +0300

    tbh I forgot what I changed

--- js/allJavaS.js
+++ js/allJavaS.js
@@ -3,16 +3,16 @@ function navToEn() {
 }
 
 // Add event listeners to the buttons
-document.getElementById("projectBtn").addEventListener("click", function() {
-document.getElementById("project").scrollIntoView({behavior: "smooth", block: "start"});
+document.getElementById("project").addEventListener("click", function() {
+document.getElementById("project-page").scrollIntoView({behavior: "smooth", block: "start"});
 });
 
-document.getElementById("aboutBtn").addEventListener("click", function() {
-document.getElementById("about").scrollIntoView({behavior: "smooth", block: "start"});
+document.getElementById("about").addEventListener("click", function() {
+document.getElementById("about-page").scrollIntoView({behavior: "smooth", block: "start"});
 });
 
-document.getElementById("contactBtn").addEventListener("click", function() {
-document.getElementById("contact").scrollIntoView({behavior: "smooth", block: "start"});
+document.getElementById("contact").addEventListener("click", function() {
+document.getElementById("contact-page").scrollIntoView({behavior: "smooth", block: "start"});
 });
 
 function updateButtonState() {
